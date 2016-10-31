ade9984cbc68ccc88a9729e5b2b4c291bbd7833d
Austin Brown <austinbrown1406@gmail.com>
2016-10-31 09:25:59 -0400

    Update index.php
    Added link to forums

--- index.php
+++ index.php
@@ -4,3 +4,5 @@ $remaining = $date - time();
 $days_remaining = floor($remaining / 86400);
 $hours_remaining = floor(($remaining % 86400) / 3600);
 echo "There are $days_remaining days and $hours_remaining hours left until the launch of BTC-Crash.";
+
+echo "As of now, however, you can visit the <a href='forum.btc-crash.ml'>forums</a> to discuss the upcoming site.";
