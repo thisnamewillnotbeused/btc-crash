ae526e1ae484d1a2b466d6666d484168d8e7640e
Austin Brown <austinbrown1406@gmail.com>
2016-10-31 08:59:24 -0400

    Create index2.php
    Add title and set multiplier increase per second

---
+++ index2.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>
+BTC-Crash Game
+</title>
+</head>
+
+
+<?php
+$mult_increase=rand(0.10, 0.20);
+
+echo "The multiplier increase per second for this game is... <b>$mult_increase</b> per second!";
+
+
+//TODO: add live graph to show multiplier and time elapsed
+
+
+
