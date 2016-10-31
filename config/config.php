9e065f68454da26a7a9cf62b5fe8f5a9ea9fd8a5
austinbro1000 <austinbrown1406@gmail.com>
2016-10-31 07:50:06 -0400

    add MySQL connection config

--- config/config.php
+++ config/config.php
@@ -1,1 +1,10 @@
+<?php
+$username = "";
+$password = "";
+$hostname = ""; 
 
+//connection to the database
+$dbhandle = mysql_connect($hostname, $username, $password) 
+  or die("Unable to connect to MySQL");
+echo "Connected to MySQL<br>";
+?>
