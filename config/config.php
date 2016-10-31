4aca00529b64af215f5767e9ec764c9a48dac20d
Austin Brown <austinbrown1406@gmail.com>
2016-10-31 08:38:15 -0400

    Update config/config.php

--- config/config.php
+++ config/config.php
@@ -3,7 +3,7 @@ $username = "";
 $password = "";
 $hostname = "";
 
-//connection to the database
+//Test connection to the database
 $dbhandle = mysql_connect($hostname, $username, $password)
   or die("Unable to connect to MySQL database");
 echo "Connected to MySQL database<br>";
